65545bf26e2b71cc469a0a9d0d32a044f87ccf82
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-15 14:04:24 +0200

    FIX

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -1,25 +1,48 @@
 {
   config,
   outputs,
+  pkgs,
   ...
 }:
 {
   imports = [
+    ./ssh.nix
+    ./nix.nix
     ./boot.nix
     ./sops.nix
     ./grub.nix
     ./home.nix
-    ./network.nix
     ./fish.nix
-    ./ssh.nix
-    ./nix.nix
     ./font.nix
     ./upower.nix
     ./nixpkgs.nix
+    ./network.nix
     ./systemd-initrd.nix
+    ./optin-persistence.nix
   ] ++ (builtins.attrValues outputs.nixosModules);
 
   # ---
+
+  security.pam.yubico = {
+    enable = true;
+    id = "31022640";
+  };
+
+  # Increase open file limit for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
 
   system.stateVersion = "${config.system.nixos.release}";
 }
