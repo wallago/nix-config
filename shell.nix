a97fec1bf261d7a183d510b9355d1aca8049b3f2
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-21 02:51:49 +0200

    add nixos-anywhere to dev env

--- shell.nix
+++ shell.nix
@@ -6,13 +6,14 @@
   default = pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
     nativeBuildInputs = with pkgs; [
-      nix # ----------> The Nix package manager
-      home-manager # -> A tool to manage user environments with Nix
-      git # ----------> The version control system
-      sops # ---------> A tool for managing secrets
-      ssh-to-age # ---> A tool for converting SSH keys to Age keys
-      gnupg # --------> The GNU Privacy Guard for encryption and signing
-      age # ----------> A simple, modern, and secure file encryption tool
+      nix # ------------> The Nix package manager
+      home-manager # ---> A tool to manage user environments with Nix
+      git # ------------> The version control system
+      sops # -----------> A tool for managing secrets
+      ssh-to-age # -----> A tool for converting SSH keys to Age keys
+      gnupg # ----------> The GNU Privacy Guard for encryption and signing
+      age # ------------> A simple, modern, and secure file encryption tool
+      nixos-anywhere # ->
     ];
   };
 }
