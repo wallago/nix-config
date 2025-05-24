3c29bbb3264841f6f12720580601a48e9fc4fe2d
wallago <45556867+wallago@users.noreply.github.com>
2025-05-24 23:23:48 +0200

    feat(hyprlock): adding finger print

--- home/feat/desktop/hyprlock.nix
+++ home/feat/desktop/hyprlock.nix
@@ -7,6 +7,7 @@ in {
   programs.hyprlock = {
     enable = true;
     settings = {
+      auth.fingerprint.enabled = true;
       general = {
         hide_cursor = true;
         ignore_empty_input = true;
