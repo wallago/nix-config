4db0816a55a455f807bcb73803c7457fb64b435d
wallago <45556867+wallago@users.noreply.github.com>
2025-05-30 11:57:09 +0200

    refact(hyprland/keybindings/screenshot): change keybindings

--- home/feat/desktop/hyprland/keybindings/screenshot.nix
+++ home/feat/desktop/hyprland/keybindings/screenshot.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, }:
 let grimblast = lib.getExe pkgs.grimblast;
 in [
-  ",Print,exec,${grimblast} --notify --freeze copy area"
-  "SHIFT,Print,exec,${grimblast} --notify --freeze copy output"
+  "SUPERSHIFT, p, exec,${grimblast} --notify --freeze copy area"
+  "SUPERSHIFTALT, p, exec,${grimblast} --notify --freeze copy output"
 ]
