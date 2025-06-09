1c4b852175c35d9d38d127377078734b3adb5027
wallago <45556867+wallago@users.noreply.github.com>
2025-06-09 16:15:06 +0200

    refactor(flake): cleanup

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "My NixOS configuration";
 
   nixConfig = {
-    bash-prompt = "[nixos-raspberrypi-demo] ➜ ";
     extra-substituters = [ "https://nix-community.cachix.org" ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
