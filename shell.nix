ae4066cfa7cd81dd810afbee7ad78b34bbd413ef
wallago <45556867+wallago@users.noreply.github.com>
2025-06-09 16:15:36 +0200

    refactor(develop): remove unused pkgs

--- shell.nix
+++ shell.nix
@@ -3,9 +3,6 @@
     NIX_CONFIG =
       "extra-experimental-features = nix-command flakes ca-derivations";
     nativeBuildInputs = with pkgs; [
-      nix
-      home-manager
-      git
       sops
       ssh-to-age
       gnupg
