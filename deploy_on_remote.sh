84a7595fdb849d035d16e986bd501e31d89503b9
wallago <45556867+wallago@users.noreply.github.com>
2025-06-09 16:30:58 +0200

    refactor: clean repo

--- deploy_on_remote.sh
+++
@@ -1,6 +0,0 @@
- nixos-anywhere switch \
-   --flake .#enma  \
-   --copy-host-keys \
-   --target-host nixos@192.168.10.116  
-   #--generate-hardware-config nixos-generate-config hosts/enma/hardware-configuration.nix \
-   # --extra-files /persistent/etc/ssh \
