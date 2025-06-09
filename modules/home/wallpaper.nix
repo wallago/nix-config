5ab1fde3dd006ba5e89b24af1d8a4735100a9b2a
wallago <45556867+wallago@users.noreply.github.com>
2025-06-09 11:42:10 +0200

    fix(wallpapers): change string to path

--- modules/home/wallpaper.nix
+++ modules/home/wallpaper.nix
@@ -1,7 +1,7 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   options.wallpaper = lib.mkOption {
-    type = lib.types.str;
-    default = "default";
+    type = lib.types.path;
+    default = pkgs.inputs.themes.wallpapers.default;
     description = ''
       Wallpaper path
     '';
