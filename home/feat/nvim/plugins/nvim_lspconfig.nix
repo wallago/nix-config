126262468806ec7a00950662664c9d2981b6268a
wallago <45556867+wallago@users.noreply.github.com>
2025-06-15 01:02:39 +0200

    refactor(neovim/plugin/lspconfig): increase log

--- home/feat/nvim/plugins/nvim_lspconfig.nix
+++ home/feat/nvim/plugins/nvim_lspconfig.nix
@@ -2,6 +2,7 @@
   plugins = with pkgs.vimPlugins; [ nvim-lspconfig ];
   config = ''
     vim.lsp.inlay_hint.enable(true)
+    vim.lsp.set_log_level("INFO")
 
     local lspconfig = require("lspconfig")
     lspconfig.nixd.setup({
