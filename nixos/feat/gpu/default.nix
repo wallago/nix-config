fdd443d601063d11d636ca0925e736fbecc47fea
wallago <45556867+wallago@users.noreply.github.com>
2025-06-19 21:41:32 +0200

    feat(nixos/feat/gpu): add vulkan-loader in 64 and 32

--- nixos/feat/gpu/default.nix
+++ nixos/feat/gpu/default.nix
@@ -2,6 +2,7 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [ vulkan-loader vulkan-validation-layers ];
+    extraPackages = with pkgs; [ vulkan-loader ];
+    extraPackages32 = with pkgs; [ vulkan-loader ];
   };
 }
