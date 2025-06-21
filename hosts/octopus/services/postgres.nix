e14b097106640d634f3d79c0586fb7435940ee79
wallago <45556867+wallago@users.noreply.github.com>
2025-06-21 18:01:09 +0200

    feat(hosts/octopus/services/postgres): allow remote conn + create rewind

--- hosts/octopus/services/postgres.nix
+++ hosts/octopus/services/postgres.nix
@@ -2,22 +2,36 @@
   services.postgresql = {
     enable = true;
     enableTCPIP = true;
-    #ensureDatabases = [ "mydatabase" ];
-    #authentication = lib.mkOverride 10 ''
-    #  #type database  DBuser  auth-method
-    #  local all       all     trust
-    #'';
-    # settings = {
-    #   listen_addresses = "*";
-    #   log_connections = true;
-    #   log_disconnections = true;
-    #   log_destination = "stderr";
-    #   logging_collector = true;
-    #   log_directory = "log";
-    #   log_filename = "postgresql-%a.log";
-    #   log_statement = "mod";
-    #   client_min_messages = "notice";
-    # };
+    authentication = lib.mkOverride 10 ''
+      #type database DBuser origin-address auth-method
+      local all      all                   trust
+      # ipv4
+      # host  all      all    127.0.0.1/32   trust
+      host  all      all    0.0.0.0/0      trust
+      # ipv6
+      # host  all      all    ::1/128        trust
+      # host  all      all    ::/0           trust
+    '';
+    settings = {
+      log_connections = true;
+      log_disconnections = true;
+      log_destination = "stderr";
+      logging_collector = true;
+      log_directory = "log";
+      log_filename = "postgresql-%a.log";
+      log_statement = "mod";
+      client_min_messages = "notice";
+    };
+    ensureDatabases = [ "rewind" ];
+    ensureUsers = [{
+      name = "rewind";
+      ensureDBOwnership = true;
+      ensureClauses = {
+        superuser = false;
+        createrole = true;
+        createdb = true;
+      };
+    }];
   };
 
   environment.persistence = {
