1ee33fb2d64b3feccacc6528b815fb45e8a54280
wallago <45556867+wallago@users.noreply.github.com>
2025-06-30 11:52:11 +0200

    refactor(waybar): rename pulseaudio to sound

---
+++ home/feat/desktop/waybar/modules/sound.nix
@@ -0,0 +1,8 @@
+{ pkgs, lib, ... }: {
+  format-source = "󰍬 {volume}%";
+  format-source-muted = "󰍭 0%";
+  format = "{icon} {volume}%   {format_source}";
+  format-muted = "󰸈 0% {format_source}";
+  format-icons = { default = [ "󰕿" "󰖀" "󰕾" ]; };
+  on-click = lib.getExe pkgs.pavucontrol;
+}
