71d6c7640888a79bb976ce7955c2ce3cf2202076
wallago <45556867+wallago@users.noreply.github.com>
2025-07-05 00:45:47 +0200

    refactor(home/hyprland): make unfocus window with full opacity

--- home/feat/desktop/hyprland/decoration/default.nix
+++ home/feat/desktop/hyprland/decoration/default.nix
@@ -1,6 +1,7 @@
 {
   active_opacity = 1.0;
-  inactive_opacity = 0.85;
+  # inactive_opacity = 0.85;
+  inactive_opacity = 1.0;
   fullscreen_opacity = 1.0;
   rounding = 7;
   blur = import ./blur.nix;
