57230d067d3f9bf4ff82837c2696591f2ba93495
wallago <45556867+wallago@users.noreply.github.com>
2025-07-14 18:45:16 +0200

    feat(feat/cli/bat): add bat plugins

--- home/feat/cli/bat.nix
+++ home/feat/cli/bat.nix
@@ -1,6 +1,13 @@
-{
+{ pkgs, ... }: {
   programs.bat = {
     enable = true;
     config.theme = "base16";
+    extraPackages = with pkgs.bat-extras; [
+      batdiff
+      batman
+      batpipe
+      batwatch
+      prettybat
+    ];
   };
 }
