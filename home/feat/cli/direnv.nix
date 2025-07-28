02e54561376d9f5df6130c4ba108b3b98587fe5f
wallago <45556867+wallago@users.noreply.github.com>
2025-07-28 20:22:59 +0200

    refactor(direnv): remove unwanted logs and add timeout on warning

--- home/feat/cli/direnv.nix
+++ home/feat/cli/direnv.nix
@@ -2,5 +2,11 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config = {
+      global = {
+        hide_env_diff = true;
+        warn_timeout = "1m";
+      };
+    };
   };
 }
