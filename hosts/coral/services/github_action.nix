f4f8b470f8fe6668038766be77d22de7d631f70f
wallago <45556867+wallago@users.noreply.github.com>
2025-11-04 22:52:01 +0100

    fix(hosts/coral): finalize setup

--- hosts/coral/services/github_action.nix
+++ hosts/coral/services/github_action.nix
@@ -1,14 +1,14 @@
 { config, ... }:
 let github-runner-rewind = config.sops.secrets."github-runner-rewind".path;
 in {
-  services.github-runners = {
-    rewind = {
-      enable = true;
-      name = "rewind";
-      tokenFile = github-runner-rewind;
-      url = "https://github.com/wallago/rewind";
-    };
-  };
+  # services.github-runners = {
+  #   rewind = {
+  #     enable = true;
+  #     name = "rewind";
+  #     tokenFile = github-runner-rewind;
+  #     url = "https://github.com/wallago/rewind";
+  #   };
+  # };
 
   sops.secrets = {
     "github-runner-rewind" = {
