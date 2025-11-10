604b7a5252da9bee12089d17fc3e3cfbad898550
wallago <45556867+wallago@users.noreply.github.com>
2025-11-10 14:34:30 +0100

    feat(nixos/feat): add regreet (login)

---
+++ nixos/feat/regreet.nix
@@ -0,0 +1,33 @@
+# Graphical greeter, hooks into greetd
+{ pkgs, ... }: {
+  programs.regreet = {
+    enable = true;
+    cageArgs = [ "-s" "-m" "last" ];
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    theme = {
+      name = "Materia-dark";
+      package = pkgs.materia-theme;
+    };
+    font = {
+      name = "Fira Sans";
+      package = pkgs.fira;
+      size = 12;
+    };
+    cursorTheme = {
+      package = pkgs.apple-cursor;
+      name = "macOS";
+    };
+  };
+
+  environment.persistence = {
+    # Persist last user and last selected session
+    "/persist".directories = [{
+      directory = "/var/lib/regreet";
+      user = "greeter";
+      group = "greeter";
+    }];
+  };
+}
