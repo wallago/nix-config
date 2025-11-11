817389e018b11d2eb9846e00b7083926de063a52
wallago <45556867+wallago@users.noreply.github.com>
2025-11-11 20:47:05 +0100

    fix(hyprland): set keybindings for brightnessctl

--- home/feat/desktop/hyprland/keybindings/brightness.nix
+++ home/feat/desktop/hyprland/keybindings/brightness.nix
@@ -1,5 +1,6 @@
-let backlight = "swayosd-client --brightness +0";
+{ pkgs, lib }:
+let backlight = lib.getExe pkgs.brightnessctl;
 in [
-  ",XF86MonBrightnessUp,exec,brightnessctl s +10%; ${backlight}"
-  ",XF86MonBrightnessDown,exec,brightnessctl s 10%-; ${backlight}"
+  ",XF86MonBrightnessUp,exec,${backlight} set +5%; swayosd-client --brightness +0"
+  ",XF86MonBrightnessDown,exec,${backlight} set 5%-; swayosd-client --brightness +0"
 ]
