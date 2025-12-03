d6297094e4f307d1d5f15c994138c56e38575e4e
wallago <45556867+wallago@users.noreply.github.com>
2025-12-03 12:02:44 +0100

    feat(nixos/gpu): enhances config

--- nixos/feat/gpu/intel.nix
+++ nixos/feat/gpu/intel.nix
@@ -1,4 +1,21 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [ ./default.nix ];
+
   services.xserver.videoDrivers = [ "intel" ];
+
+  hardware.graphics = {
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-compute-runtime
+      intel-vaapi-driver
+      libva-vdpau-driver
+      libvdpau-va-gl
+      vulkan-loader
+      intel-ocl
+    ];
+    extraPackages32 = with pkgs; [ vulkan-loader intel-vaapi-driver ];
+  };
+
+  boot.kernelParams = [ "i915.enable_fbc=1" "i915.fastboot=1" ];
+  boot.initrd.kernelModules = [ "i915" ];
 }
