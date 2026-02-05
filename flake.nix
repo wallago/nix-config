ce85351dd0682fd08ad2d58cd322f60d2abb368d
wallago <45556867+wallago@users.noreply.github.com>
2026-02-05 07:07:35 +0100

    feat(flake): add gateway in inputs

--- flake.nix
+++ flake.nix
@@ -55,6 +55,7 @@
     rewind-frontend.url = "git+ssh://git@github.com/wallago/rewind?dir=front/nix";
     markeeper-backend.url = "git+ssh://git@github.com/wallago/markeeper?dir=back/nix";
     markeeper-frontend.url = "git+ssh://git@github.com/wallago/markeeper?dir=front/nix";
+    gateway.url = "git+ssh://git@github.com/tools-hood/gateway?dir=nix";
   };
 
   outputs =
