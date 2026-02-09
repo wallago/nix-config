900fc36bf5e3bde1709925ed2ffcf84ea6dc1dc1
wallago <45556867+wallago@users.noreply.github.com>
2026-02-09 08:46:51 +0100

    chore(shell): format

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
-{ pkgs ? import <nixpkgs> { }, inputs, ... }:
+{
+  pkgs ? import <nixpkgs> { },
+  inputs,
+  ...
+}:
 let
-in {
+in
+{
   default = pkgs.mkShell {
-    NIX_CONFIG =
-      "extra-experimental-features = nix-command flakes ca-derivations";
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
     buildInputs = [
       pkgs.sops
       pkgs.ssh-to-age
@@ -15,15 +19,15 @@ in {
       inputs.nix-deployer.packages.${pkgs.system}.default
     ];
     shellHook = ''
-      ${
-        inputs.project-banner.packages.${pkgs.system}.default
-      }/bin/project-banner \
+      ${inputs.project-banner.packages.${pkgs.system}.default}/bin/project-banner \
         --owner "wallago" \
         --logo " 󰖌 " \
         --product "Nixos" \
         --part "config" \
         --code "WL24-NIXO-CF01" \
-        --tips "sops updatekeys <path to sops file>"
+        --tips "add SOPS_AGE_KEY=\$(ssh-to-age -private-key -i ~/.ssh/id_ed25519) if no yubikey" \
+        --tips "sops updatekeys <path to sops file>" \
+        --tips "sops <path to sops file>"
     '';
   };
 }
