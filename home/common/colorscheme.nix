86995445d5604eb9aee412ad3a8d9b795b93aceb
wallago <45556867+wallago@users.noreply.github.com>
2026-02-23 15:37:05 +0100

    feat(home/specialisation): add wallpaper + mode dynamic loading

--- home/common/colorscheme.nix
+++ home/common/colorscheme.nix
@@ -1,57 +1,79 @@
-{ lib, config, pkgs, ... }: {
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  wallpapers = inputs.themes.packages.${pkgs.system}.wallpapers;
+  tofi = lib.getExe' config.programs.tofi.package "tofi";
+in
+{
   colorscheme.mode = lib.mkOverride 1499 "dark";
 
-  specialisation = {
-    dark.configuration.colorscheme.mode = lib.mkOverride 1498 "dark";
-    light.configuration.colorscheme.mode = lib.mkOverride 1498 "light";
-  };
+  specialisation =
+    let
+      modes = [
+        "dark"
+        "light"
+      ];
+    in
+    lib.listToAttrs (
+      lib.flatten (
+        map (
+          mode:
+          lib.mapAttrsToList (name: wp: {
+            name = "${name}-${mode}";
+            value.configuration = {
+              wallpaper = lib.mkForce wp;
+              colorscheme.mode = lib.mkOverride 1498 mode;
+            };
+          }) wallpapers
+        ) modes
+      )
+    );
 
-  home.file = {
-    ".colorscheme.json".text = builtins.toJSON config.colorscheme;
-  };
+  home.file.".colorscheme.json".text = builtins.toJSON config.colorscheme;
 
-  home.packages = let
-    specialisation = pkgs.writeShellScriptBin "specialisation" ''
-      profiles="$HOME/.local/state/nix/profiles"
-      current="$profiles/home-manager"
-      base="$profiles/home-manager-base"
+  home.packages =
+    let
+      specialisation = pkgs.writeShellScriptBin "specialisation" ''
+        profiles="$HOME/.local/state/nix/profiles"
+        current="$profiles/home-manager"
+        base="$profiles/home-manager-base"
+        current_home="$HOME/.local/state/home-manager/gcroots/current-home" # For NixOS
 
-      # If current contains specialisations, link it as base
-      if [ -d "$current/specialisation" ]; then
-        echo >&2 "Using current profile as base"
-        ln -sfT "$(readlink "$current")" "$base"
-      # Check that $base contains specialisations before proceeding
-      elif [ -d "$base/specialisation" ]; then
-        echo >&2 "Using previously linked base profile"
-      else
-        echo >&2 "No suitable base config found. Try 'home-manager switch' again."
-        exit 1
-      fi
-
-      if [ -z "$1" ] || [ "$1" = "list" ] || [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
-        find "$base/specialisation" -type l -printf "%f\n"
-        exit 0
-      fi
-
-      echo >&2 "Switching to ''${1} specialisation"
-      if [ "$1" == "base"  ]; then
-        "$base/activate"
-      else
-        "$base/specialisation/$1/activate"
-      fi
-    '';
-    toggle-theme = pkgs.writeShellScriptBin "toggle-theme" ''
-      if [ -n "$1" ]; then
-        theme="$1"
-      else
-        current="$(${lib.getExe pkgs.jq} -re '.mode' "$HOME/.colorscheme.json")"
-        if [ "$current" = "light" ]; then
-          theme="dark"
+        # Try to get it from NixOS, if so, link it
+        if [ -d "$current_home/specialisation" ]; then
+          echo >&2 "Using current-home gcroot"
+          ln -sfT "$(readlink "$current_home")" "$base"
+        # If current contains specialisations, link it as base
+        elif [ -d "$current/specialisation" ]; then
+          echo >&2 "Using current profile as base"
+          ln -sfT "$(readlink "$current")" "$base"
+        # Check that $base contains specialisations before proceeding
+        elif [ -d "$base/specialisation" ]; then
+          echo >&2 "Using previously linked base profile"
         else
-          theme="light"
+          echo >&2 "No suitable base config found. Try 'home-manager switch' or 'nixos-rebuild switch' again."
+          exit 1
         fi
-      fi
-      ${lib.getExe specialisation} "$theme"
-    '';
-  in [ specialisation toggle-theme ];
+
+        if [ -z "$1" ] || [ "$1" = "list" ] || [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
+          find "$base/specialisation" -type l -printf "%f\n"
+          exit 0
+        fi
+
+        echo >&2 "Switching to ''${1} specialisation"
+        if [ "$1" == "base"  ]; then
+          "$base/activate"
+        else
+          "$base/specialisation/$1/activate"
+        fi
+      '';
+    in
+    [
+      specialisation
+    ];
 }
--- home/feat/desktop/hyprland/keybindings/launcher.nix
+++ home/feat/desktop/hyprland/keybindings/launcher.nix
@@ -1,10 +1,11 @@
-{ lib, config, }:
+{ lib, config }:
 let
   tofidrun = lib.getExe' config.programs.tofi.package "tofi-drun";
   tofirun = lib.getExe' config.programs.tofi.package "tofi-run";
   tofi = lib.getExe' config.programs.tofi.package "tofi";
-in lib.optionals config.programs.tofi.enable [
+in
+lib.optionals config.programs.tofi.enable [
   "SUPER,x,exec,${tofidrun} --drun-launch=true"
   "SUPER,d,exec,${tofirun}"
-  "SUPER,t,exec,specialisation=$(specialisation | ${tofi})"
+  "SUPER,t,exec,specialisation --list | tofi | xargs specialisation"
 ]
