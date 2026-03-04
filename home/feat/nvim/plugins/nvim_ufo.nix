0afaadbbecf48590d433e8de07ced19edc4f2a8c
wallago <45556867+wallago@users.noreply.github.com>
2026-03-04 10:07:39 +0100

    refactor(nvim/ufo): remove mapping

--- home/feat/nvim/plugins/nvim_ufo.nix
+++ home/feat/nvim/plugins/nvim_ufo.nix
@@ -10,16 +10,5 @@
         return { "lsp", "indent" }  -- LSP first, fallback to indent
       end,
     })
-
-    vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-    vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-    vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
-    vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
-    vim.keymap.set("n", "K", function()
-      local winid = require("ufo").peekFoldedLinesUnderCursor()
-      if not winid then
-        vim.cmd("Lspsaga hover_doc")
-      end
-    end, { desc = "Peek fold or hover" })
   '';
 }
