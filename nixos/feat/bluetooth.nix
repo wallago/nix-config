fc579958adb7090fa55e76f8c5e284484535a97b
wallago <45556867+wallago@users.noreply.github.com>
2026-04-16 16:20:37 +0200

    feat(bluetooth): add some new tools (better workflows)

--- nixos/feat/bluetooth.nix
+++ nixos/feat/bluetooth.nix
@@ -1,6 +1,13 @@
+{ pkgs, ... }:
 {
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    package = pkgs.bluez;
   };
+
+  environment.systemPackages = with pkgs; [
+    bluez-tools
+    bluetuith
+  ];
 }
