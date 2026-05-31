083f1b821c3b7c52b1984cbab0809b567c8d9c80
wallago <45556867+wallago@users.noreply.github.com>
2026-05-31 13:51:33 +0200

    wip(workflows): work on ci/cd

--- modules/features/networking/ssh.nix
+++ modules/features/networking/ssh.nix
@@ -13,34 +13,35 @@
     networking.firewall.allowedTCPPorts = [ 2222 ];
   };
 
-  flake.homeModules.ssh = {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      settings."*" = {
-        hashKnownHosts = true;
-        serverAliveInterval = 60;
+  flake.homeModules = {
+    ssh = {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+        settings."*" = {
+          hashKnownHosts = true;
+          serverAliveInterval = 60;
+        };
       };
     };
-  };
-
-  flake.homeModules.sshWg0 = {
-    programs.ssh.settings = {
-      coral = {
-        hostname = "10.100.0.1";
-        port = 2222;
-        user = "wallago";
-        setEnv = "TERM=xterm-256color";
-      };
-      sponge = {
-        hostname = "10.100.0.3";
-        port = 2222;
-        user = "wallago";
-      };
-      squid = {
-        hostname = "10.100.0.3";
-        port = 2222;
-        user = "wallago";
+    sshWg0 = {
+      programs.ssh.settings = {
+        coral = {
+          hostname = "10.100.0.1";
+          port = 2222;
+          user = "wallago";
+          setEnv = "TERM=xterm-256color";
+        };
+        sponge = {
+          hostname = "10.100.0.3";
+          port = 2222;
+          user = "wallago";
+        };
+        squid = {
+          hostname = "10.100.0.3";
+          port = 2222;
+          user = "wallago";
+        };
       };
     };
   };
