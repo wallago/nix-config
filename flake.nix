222c41452466359786252e53d44521b101b48337
wallago <45556867+wallago@users.noreply.github.com>
2026-06-04 16:34:17 +0200

    fix(cd): bump info for attic deployment

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "mycache:W6wlRqokUMPvvM6Mo9UKnb4iBOsv3IbAC5TVPCtVnPY="
+      "mycache:nsIUEPic0Apzfl+dUwiAcU9e8r2/maPx50IjfyHkRPg="
       "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="
     ];
     connect-timeout = 5;
