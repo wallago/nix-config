e9b2c111332a1903f8002693b0be49b3520e126a
wallago <45556867+wallago@users.noreply.github.com>
2026-06-10 20:18:06 +0200

    feat(syncthing): add squid metadata

--- modules/hosts/coral/preferences/syncthing.nix
+++ modules/hosts/coral/preferences/syncthing.nix
@@ -11,7 +11,10 @@
             id = "VU4ODZ4-RJGOBR7-KQO32MG-IPQCFLB-AFH3SCZ-XHWUSVY-YD32PYN-4S2GPQR";
             addresses = [ "tcp://10.100.0.3:22000" ];
           };
-          squid.id = "";
+          squid = {
+            id = "SARRURG-4MGEJY6-7I6E4WG-KNF2Y2M-H7AO7A6-VPZA66Z-BWANK2E-6WMY3A3";
+            addresses = [ "tcp://10.100.0.2:22000" ];
+          };
           worm = {
             id = "VECK7WJ-R4CXSHS-RWOGRLH-RTBA42P-M2SZKEY-AIBSAEQ-A3U5QYG-UNY6WAB";
             addresses = [ "tcp://10.100.0.6:22000" ];
--- modules/hosts/sponge/preferences/syncthing.nix
+++ modules/hosts/sponge/preferences/syncthing.nix
@@ -11,7 +11,10 @@
             id = "DTVJZPN-EZ4LYVA-AXHJGJ2-RX7HV2B-7JSVDIB-OMRKK6X-YJZMKDY-RVCKOQL";
             addresses = [ "tcp://10.100.0.1:22000" ];
           };
-          squid.id = "";
+          squid = {
+            id = "SARRURG-4MGEJY6-7I6E4WG-KNF2Y2M-H7AO7A6-VPZA66Z-BWANK2E-6WMY3A3";
+            addresses = [ "tcp://10.100.0.2:22000" ];
+          };
           worm = {
             id = "VECK7WJ-R4CXSHS-RWOGRLH-RTBA42P-M2SZKEY-AIBSAEQ-A3U5QYG-UNY6WAB";
             addresses = [ "tcp://10.100.0.6:22000" ];
