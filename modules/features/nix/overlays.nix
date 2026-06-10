fcc2ee653fa37f613a72ad979aae9cfbac523b6b
wallago <45556867+wallago@users.noreply.github.com>
2026-06-10 22:55:55 +0200

    refactor: deadnix

--- modules/features/nix/overlays.nix
+++ modules/features/nix/overlays.nix
@@ -1,7 +1,7 @@
 {
   flake.nixosModules.nixOverlays = {
     nixpkgs.overlays = [
-      (final: prev: {
+      (_: prev: {
         kulala-core = prev.kulala-core.overrideAttrs (old: {
           node_modules = old.node_modules.overrideAttrs (_: {
             outputHash = "sha256-XQlBawD3vt8pVc7Gy9XeiGie89HWbljNJt7kUEDaDKk=";
