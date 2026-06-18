10757be0b9447d3da30a718226ddd4c214090a05
wallago <45556867+wallago@users.noreply.github.com>
2026-06-18 16:36:55 +0200

    feat(bufferline): add a map to close buffer and go prev

--- modules/features/developer/editor/nvim/plugins/bufferline.nix
+++ modules/features/developer/editor/nvim/plugins/bufferline.nix
@@ -30,6 +30,26 @@
             vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>")
             vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<CR>")
             vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
+
+            -- Smart close: drop to previous buffer, or close the split if it
+            -- was the last buffer in it
+            local function close_buffer()
+              local cur = vim.api.nvim_get_current_buf()
+              local listed = vim.tbl_filter(function(b)
+                return vim.bo[b].buflisted
+              end, vim.api.nvim_list_bufs())
+
+              if #listed > 1 then
+                vim.cmd("BufferLineCyclePrev")
+                vim.cmd("bdelete " .. cur)
+              elseif vim.fn.winnr("$") > 1 then
+                vim.cmd("close")
+              else
+                vim.cmd("bdelete")
+              end
+            end
+
+            vim.keymap.set("n", "<leader>bd", close_buffer, { desc = "Buffer: close" })
           '';
         }
       ];
