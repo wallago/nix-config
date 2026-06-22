e11df143af81deeb8b90323eaefb2cc5739ad291
wallago <45556867+wallago@users.noreply.github.com>
2026-06-22 13:05:38 +0200

    fix(nvidia): update packages to latest

--- modules/features/gpu/nvidia.nix
+++ modules/features/gpu/nvidia.nix
@@ -17,7 +17,7 @@
         # Enable the Nvidia settings menu,
         # accessible via `nvidia-settings`.
         nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.production;
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
       };
     };
 }
