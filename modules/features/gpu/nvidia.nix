8a29f4bcc359c6bf6e47e01ed1b38ff9da864745
wallago <45556867+wallago@users.noreply.github.com>
2026-06-29 17:52:59 +0200

    fix(gpu/nvidia): stop driver to crash randomly

--- modules/features/gpu/nvidia.nix
+++ modules/features/gpu/nvidia.nix
@@ -9,15 +9,31 @@
 
       services.xserver.videoDrivers = [ "nvidia" ];
 
+      boot.initrd.kernelModules = [
+        "nvidia"
+        "nvidia_modeset"
+        "nvidia_uvm"
+        "nvidia_drm"
+      ];
+
       hardware.nvidia = {
         modesetting.enable = true;
-        powerManagement.enable = false;
+        powerManagement.enable = true;
         powerManagement.finegrained = false;
         open = true;
         # Enable the Nvidia settings menu,
         # accessible via `nvidia-settings`.
         nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
+        # package = config.boot.kernelPackages.nvidiaPackages.latest;
+        videoAcceleration = true;
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "595.84";
+          sha256_64bit = "sha256-mcQE5SExvye8ptoCaNzOPr7cenOrF0BxqZXPGmxeugY=";
+          sha256_aarch64 = "sha256-GloNdDFfmXFVu4FAlNNk2qzqLOuw2N5CKatKkcSrQxk=";
+          openSha256 = "sha256-pEmA2tUcOKwUPKy6N0QvS49Pdut4/7Phs/JhjdyBcNY=";
+          settingsSha256 = "sha256-QrnBM+sdWO4GanO62rxpHmRrjYkYpl5RD6fIiHq4C4A=";
+          persistencedSha256 = "sha256-50xYdgx7EEThbaMp4QS8GADbxj0mhBXh8QQN0tWMwRg=";
+        };
       };
     };
 }
