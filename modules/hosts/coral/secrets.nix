d3b5ffd46795519d94ead09db9d6d7719b889fa5
wallago <45556867+wallago@users.noreply.github.com>
2026-07-09 11:56:49 +0200

    refactor(secrets): creates modules for each secrets common setup

--- modules/hosts/coral/secrets.nix
+++ modules/hosts/coral/secrets.nix
@@ -1,33 +1,19 @@
-{
-  flake.nixosModules.secretsCoral =
-    { config, lib, ... }:
-    let
-      ifaces = config.preferences.wireguard.server.interfaces;
-    in
-    {
-      sops.secrets = lib.mkMerge [
-        {
-          wallago-password.neededForUsers = true;
-        }
-        (lib.mapAttrs' (name: _: {
-          name = "${name}-sk";
-          value = { };
-        }) ifaces)
-        {
-          syncthing-password.owner = config.services.syncthing.user;
-          syncthing-cert = { };
-          syncthing-key = { };
-          miniflux-credentials = { };
-          "wallago.xyz-ssl-crt" = {
-            owner = config.services.nginx.user;
-            group = config.services.nginx.group;
-          };
-          "wallago.xyz-ssl-key" = {
-            owner = config.services.nginx.user;
-            group = config.services.nginx.group;
-          };
-          atticd-env-keys = { };
-        }
-      ];
+{ self, ... }: {
+  flake.nixosModules.secretsCoral = { config, ... }: {
+
+    imports = [
+      self.nixosModules.secretsSyncthing
+      self.nixosModules.secretsMiniflux
+      self.nixosModules.secretsWireguardServer
+      self.nixosModules.secretsUser
+      self.nixosModules.secretsAttic
+    ];
+
+    sops.secrets = {
+      "wallago.xyz-ssl-key" = {
+        owner = config.services.nginx.user;
+        group = config.services.nginx.group;
+      };
     };
+  };
 }
--- modules/hosts/provision-iso/secrets.nix
+++ modules/hosts/provision-iso/secrets.nix
@@ -1,32 +1,8 @@
-{
-  flake.nixosModules.secretsProvisionIso =
-    { config, lib, ... }:
-    let
-      ifaces = config.preferences.wireguard.client.interfaces;
-      wg-server = "vpn.wallago.xyz";
-    in
-    {
-      sops = {
-        secrets = lib.mkMerge [
-          (lib.mapAttrs' (name: _: {
-            name = "${name}-sk";
-            value = { };
-          }) ifaces)
-        ];
-        templates = lib.mapAttrs' (name: iface: {
-          name = "${name}.conf";
-          value.content = ''
-            [Interface]
-            PrivateKey = ${config.sops.placeholder."${name}-sk"}
-            Address = ${iface.ip}
-
-            [Peer]
-            PublicKey = ${iface.serverPublicKey}
-            AllowedIPs = ${lib.concatStringsSep ", " iface.allowedIPs}
-            Endpoint = ${wg-server}:${toString iface.serverPort}
-            PersistentKeepalive = 25
-          '';
-        }) ifaces;
-      };
-    };
+{ self, ... }: {
+  flake.nixosModules.secretsProvisionIso = {
+    imports = [
+      self.nixosModules.secretsWireguardClient
+      self.nixosModules.secretsUser
+    ];
+  };
 }
