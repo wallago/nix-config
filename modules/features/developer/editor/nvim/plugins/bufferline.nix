bf0c9d37ee7f49d1d8f50aed24728aaed9a18710
wallago <45556867+wallago@users.noreply.github.com>
2026-07-13 20:32:21 +0200

    fix(nvim/bufferline): change back bindings

--- modules/features/developer/editor/nvim/plugins/bufferline.nix
+++ modules/features/developer/editor/nvim/plugins/bufferline.nix
@@ -27,10 +27,8 @@
             -- Most used functions
             vim.keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<CR>")
             vim.keymap.set("n", "<leader>bf", "<cmd>BufferLineCycleNext<CR>")
-            -- vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>")
-            vim.keymap.set("n", "<leader>bf", "<cmd>BufferLineCycleNext<CR>")
-            vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<CR>")
-            -- vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
+            vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>")
+            vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>")
             vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<CR>")
 
             -- Smart close: drop to previous buffer, or close the split if it
