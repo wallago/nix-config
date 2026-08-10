ca980476290e6137f1bf9fd63c594aaa0a7b830a
wallago <45556867+wallago@users.noreply.github.com>
2026-08-10 14:14:08 +0200

    feat(praline): adding settings

--- modules/features/developer/praline.nix
+++ modules/features/developer/praline.nix
@@ -4,7 +4,16 @@
 
     programs.praline = {
       enable = true;
-      settings = { };
+      settings = {
+        keybindings = {
+          scroll_down = "e";
+          scroll_up = "i";
+          scroll_right = "o";
+          scroll_left = "n";
+          generate = "g";
+          quit = "q";
+        };
+      };
     };
   };
 }
