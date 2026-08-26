64c5ddc52c1d32fc79eb6ee6ef8d535cb86ecf74
wallago <45556867+wallago@users.noreply.github.com>
2026-08-26 11:12:15 +0200

    fix(kicad): add x11 env setup to make it work smooth

--- modules/features/developer/dev-apps/kicad.nix
+++ modules/features/developer/dev-apps/kicad.nix
@@ -2,8 +2,17 @@
   flake.nixosModules.kicad =
     { pkgs, ... }:
     {
-      environment.systemPackages = with pkgs; [
-        kicad
+      environment.systemPackages = [
+        (pkgs.symlinkJoin {
+          name = "kicad-x11";
+          paths = [ pkgs.kicad ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            for bin in $out/bin/*; do
+              wrapProgram "$bin" --set GDK_BACKEND x11
+            done
+          '';
+        })
       ];
     };
 }
