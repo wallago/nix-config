d2cfbfb0f8adaf0c97d128891d2de743dc66da34
wallago <45556867+wallago@users.noreply.github.com>
2026-09-19 09:52:05 +0200

    refactor(squid/session): match changes of niri apps

--- modules/hosts/squid/preferences/session.nix
+++ modules/hosts/squid/preferences/session.nix
@@ -1,107 +1,51 @@
-{
+{ self, ... }: {
   flake.homeModules.preferencesSessionSquid =
+    { pkgs, lib, ... }:
     let
-      notesCmd = [
-        "ghostty"
-        "--title=notes"
-        "--working-directory=/home/wallago"
-        "-e"
-        "vi"
-        "./Notes"
-        "-c"
-        "lua require('telekasten').goto_thisweek({ journal_auto_open = true })"
-        "-c"
-        "vsplit ./Notes/pickup-tasks.md"
-      ];
-      webSecondary = [
-        "zen-beta"
-        "--name=zen-secondary"
-        "--class=zen-secondary"
-        "-P"
-        "secondary"
-        "--new-instance"
-      ];
-      rssCmd = [
-        "ghostty"
-        "--title=rss"
-        "-e"
-        "eilmeldung"
-      ];
-      webMainCmd = [
-        "zen-beta"
-        "--name=zen-main"
-        "--class=zen-main"
-        "--new-instance"
-      ];
-      mailCmd = [
-        "ghostty"
-        "--title=matcha"
-        "-e"
-        "matcha"
-      ];
-      expenseTrackerCmd = [
-        "ghostty"
-        "--title=bagels"
-        "-e"
-        "bagels"
-      ];
-      homeTermCmd = [
-        "ghostty"
-        "--title=home"
-        "--working-directory=/home/wallago/"
-        "-e"
-        "vi"
-        "./sync-pm/gateway.md"
-      ];
+      app = self.lib.app.command { inherit pkgs lib; };
     in
     {
       preferences.session = [
         {
-          command = notesCmd;
-          matchAppId = "com.mitchellh.ghostty";
+          command = app.webSecondary;
+          matchAppId = "zen.static";
+          workspace = "browser";
+          maximized = true;
+        }
+        {
+          command = app.todo;
+          matchAppId = "com.slot.todo";
+          matchTitle = "todo";
+          key = "1";
+        }
+        {
+          command = app.note;
+          matchAppId = "com.slot.notes";
           matchTitle = "notes";
-          workspace = "productivity";
-          maximized = true;
+          key = "2";
         }
         {
-          command = webSecondary;
-          matchAppId = "zen-secondary";
-          workspace = "browser";
-          maximized = true;
-        }
-        {
-          command = webMainCmd;
-          matchAppId = "zen-main";
-          workspace = "browser";
-          maximized = true;
-        }
-        {
-          command = rssCmd;
-          matchAppId = "com.mitchellh.ghostty";
-          matchTitle = "rss";
-          workspace = "productivity";
-          maximized = true;
-        }
-        {
-          command = homeTermCmd;
-          matchAppId = "com.mitchellh.ghostty";
-          matchTitle = "home";
-          workspace = "home";
-          maximized = true;
-        }
-        {
-          command = mailCmd;
-          matchAppId = "com.mitchellh.ghostty";
+          command = app.mail;
+          matchAppId = "com.slot.mail";
           matchTitle = "matcha";
-          workspace = "productivity";
-          maximized = true;
+          key = "3";
         }
         {
-          command = expenseTrackerCmd;
-          matchAppId = "com.mitchellh.ghostty";
-          matchTitle = "bagels";
-          workspace = "productivity";
-          maximized = true;
+          command = app.webMain;
+          matchAppId = "com.slot.zen.main";
+          key = "4";
+        }
+        {
+          command = app.resources;
+          matchAppId = "com.slot.resources";
+          matchTitle = "resources";
+          key = "9";
+        }
+        {
+          command = app.term;
+          matchAppId = "com.slot.term";
+          matchTitle = "term";
+          key = "0";
         }
       ];
     };
